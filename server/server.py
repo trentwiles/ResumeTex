0414eaa8c8998f224564f63d52c03cdeecd334ed
Anish Sahoo <anishsahoo2005@gmail.com>
2024-08-07 20:59:14 -0400

    Added flask server

---
+++ server/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello():
+    return 'Hello, world!'
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = request.get_json()
+    # Process the received JSON data
+    # ...   
+    return 'Received JSON data successfully'
+
+if __name__ == '__main__':
+    app.run()
