c45ddbc5316e5a4e7a3c96d8d744b8de690a4774
Anish Sahoo <anishsahoo2005@gmail.com>
2024-12-16 19:27:50 -0500

    Add code

--- server.py
+++ server.py
@@ -21,35 +21,41 @@ def cleanup():
 
 @app.get("/")
 async def root():
-    return FileResponse("public/index.html")
+    try:
+        return FileResponse("public/index.html")
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.post("/api/v1/text")
 async def predict(data: dict):
-    resume = ResumeMaker()
-    return resume.get_complete_latex(data)
+    try:
+        resume = ResumeMaker()
+        return resume.get_complete_latex(data)
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.post("/api/v1/tex")
 async def predict(data: dict):
-    resume = ResumeMaker()
-    latex_content = resume.get_complete_latex(data)
-    return FileResponse(
-        content=latex_content.encode(), media_type="text/plain", filename="resume.tex"
-    )
+    try:
+        resume = ResumeMaker()
+        latex_content = resume.get_complete_latex(data)
+        return FileResponse(
+            content=latex_content.encode(), media_type="text/plain", filename="resume.tex"
+        )
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.post("/api/v1/pdf")
 async def get_pdf(data: dict, background_tasks: BackgroundTasks):
-    resume = ResumeMaker()
     try:
+        resume = ResumeMaker()
         pdf_path = resume.generate_pdf(data)
-    except RuntimeError as e:
-        raise HTTPException(status_code=500, detail=f"Failed to generate PDF: {str(e)}")
-
-    background_tasks.add_task(cleanup)
-    response = FileResponse(
-        path=pdf_path, media_type="application/pdf", filename="resume.pdf"
-    )
-
-    return response
+        background_tasks.add_task(cleanup)
+        return FileResponse(
+            path=pdf_path, media_type="application/pdf", filename="resume.pdf"
+        )
+    except Exception as e:
+        return {"error": str(e)}
